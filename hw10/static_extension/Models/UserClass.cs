2de393c9cd03089dda2bb43218186b898038697b
Samirza0912 <samirza@code.edu.az>
2022-03-29 17:28:47 +0400

    bug fixes

---
+++ hw10/static_extension/Models/UserClass.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace static_extension.Models
+{
+    public class UserClass : IAccount
+    {
+        
+        private string _Password;
+
+        public int Id { get; set; }
+        public string Fullname { get; set; }
+        public string Email { get; set; }
+        public string Password { get { return _Password; } set
+            {
+                if (value.Length >= 8 && Cases(value) == true)
+                {
+                    Console.WriteLine("password is set");
+
+                    return;
+                }
+                Console.WriteLine("the length of password cannot be less than 8 symbols, " +
+                        "you should type at least 1 upper symbol, " +
+                        "1 lower symbol and number");
+                return;
+            } }
+        public void use (string fullname, string email, string password)
+        {
+            Fullname = fullname;
+            Email = email;
+            Password = password;
+
+        }
+        
+         public void PasswordChecker(string password)
+        {
+
+       
+        }
+        public bool Cases(string pass)
+        {
+            bool res1 = false;
+            bool res2 = false;
+            bool res3 = false;
+            for (int i = 0; i < pass.Length; i++)
+            {
+                if (char.IsUpper(pass[i]) == true)
+                {
+                    res2 = true;
+                }
+                if (char.IsNumber(pass[i]) == true)
+                {
+                    res1 = true;
+                }
+                if (char.IsLower(pass[i]) == true)
+                {
+                    res3 = true;
+                }
+            }
+            bool res = res1 && res2 && res3;
+            return res;
+
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{Id} {Fullname} {Email}");
+        }
+    }
+}
---
+++ hw10/static_extension/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using static_extension.Models;
+
+namespace static_extension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IAccount (interface):
+            //- PasswordChecker() - parametr olaraq string password qəbul edir.
+            //- ShowInfo()
+            //User class (IAccount-u implement edir)
+            //- Id
+            //- Fullname
+            //- Email
+            //- Password
+            //- PasswordChecker() - PasswordChecker methodu -
+            //gələn string password dəyərinin şərtləri ödəyib ödəmədiyini yoxlayıb true/false dəyər qaytarir.
+            //Şərtlər:
+            //                        - şifrədə minimum 8 character olmalıdır
+            //                        - şifrədə ən az 1 böyük hərf olmalıdır
+            //                        - şifrədə ən az 1 kiçik hərf olmalıdır
+            //                        - şifrədə ən az 1 rəqəm olmalıdır
+            //- ShowInfo() - bu method console-a user-in Id, Fullname və email dəyərlərini yazdırır
+            //ps: Id dəyəri hər dəfə bir user obyekti yaranan zaman avtomatik artmalıdır
+            //            və qıraqdan id dəyərini dəyişmək olmamalıdı ancaq get etmək olar.
+            //            User yarandığı zaman email və password təyin edilməsi məcburidir.
+            //            User-ə şifrə təyin edilərkən şifrənin PasswordChecker methodunun şərtlərini ödəməsi lazımdır.
+
+            do
+            {
+                UserClass user = new UserClass();
+                Console.Clear();
+                Console.WriteLine("please enter your ID");
+                user.Id = int.Parse(Console.ReadLine());
+                Console.WriteLine("please enter your fullname");
+                user.Fullname = (Console.ReadLine());
+                Console.WriteLine("please enter your email");
+                user.Email = (Console.ReadLine());
+                Console.WriteLine("please enter your password");
+                user.Password = (Console.ReadLine());
+
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+
+        }
+    }
+    interface IAccount
+    {
+        public void PasswordChecker(string password);
+        public void ShowInfo();
+
+    }
+}
